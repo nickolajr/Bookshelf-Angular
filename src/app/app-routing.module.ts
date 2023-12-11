904b21397a4b1321ec4934a4f97dab77507cc826
TrickFire <36343043+nickolajr@users.noreply.github.com>
2023-12-11 11:39:15 -0800

    yes | cp -rf ./src/app/app-routing.module.ts ./node_modules/@ionic/angular/dist/app-routing.module.d.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,7 @@ const routes: Routes = [
     { path: 'change-email', component: ChangeEmailComponent },
     { path: 'change-username', component: ChangeUsernameComponent },
   ] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
