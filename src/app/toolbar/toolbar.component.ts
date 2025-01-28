dafc27e0c0a9e697ddce6738da753c76a76dcc25
TrickFire <36343043+nickolajr@users.noreply.github.com>
2025-01-28 14:39:14 +0100

    logout

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,23 +1,40 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Account } from '../models/Account';
+import { LoginService } from '../services/login.service';
+import { Subscription, take  } from 'rxjs';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent {
-  isLoggedIn: boolean = false;
 
-  constructor(private router: Router) {}
 
+  constructor(private router: Router,private loginService:LoginService) {}
+  public account: Account | null = null;
+  private accountSubscription: Subscription | undefined;
+  public isLoggedIn: boolean = false;
   ngOnInit(): void {
-    this.isLoggedIn = sessionStorage.getItem('accountId') != null;
-  }
+
+    this.accountSubscription = this.loginService.currentUser$
+      .pipe(take(1))
+      .subscribe({
+        next: (account) => {
+          this.account = account;
+          this.isLoggedIn = this.loginService.convertToBoolean(this.account?.isLoggedin);
+        },
+        error: (error) => {
+          console.error('Error fetching user account:', error);
+          
+        },
+      });
+    }
+  
 
   logout() {
     // Remove the user's session data
-    sessionStorage.removeItem('accountId');
+    this.loginService.logout();
 
     // Redirect to the login page
     window.location.href = '/login';
